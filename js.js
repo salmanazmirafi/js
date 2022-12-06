aa6d8a919203dbcd2f8374e05d40c334e5a6d928
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-12-06 13:50:32 +0600

    last

--- js.js
+++ js.js
@@ -1,3 +1,27 @@
 let name = "Hello World";
 
 console.log(name);
+
+//===================================
+/**
+ * Javascript Basic
+ * ----------------
+ *
+ * Javascript Data type
+ * String
+ * Number
+ * Boleyn
+ * Primitive
+ *
+ */
+//===================================
+/**
+ * Javascript Function
+ *
+ */
+const myFunction = function () {
+  console.log("Function MY Function");
+};
+myFunction();
+//===================================
+//===================================
