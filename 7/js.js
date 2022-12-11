bf46288cb6b9f649b7adf95f1c9bb0e91d56d9cc
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-11 15:58:41 +0600

    Update

--- 7/js.js
+++ 7/js.js
@@ -3,4 +3,23 @@
 // Object As Data Structure
 // Multi Dime-nt Array
 
-8:minit
+//const number = [2,3,4,5,6,67,7,8,22,44,5,3,4,554,6,67,]
+//number.forEach(function(value){
+//if(value % 2 === 0){
+//console.log(value)
+//})
+
+//const number = [2,3,4,5,6,67,7,8,22,44,5,3,4,554,6,67,]
+//let nu = 0
+//number.forEach(function(value,i){
+//if (i < 3){}
+//nu += value
+//})
+//console.log(nu)
+
+const name = ["Salman","Azmi","Sonia",2,4,6,7,3,1,false,true,null]
+
+
+
+
+console.log(name)
